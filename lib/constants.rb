e0f49ac9959980902ef3faa7ca320fc5aa5b6999
Les Nightingill <codehacker@comcast.net>
2012-03-18 17:06:34 -0700

    change APPLICATION_NAME so it reads better on the pages

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,5 +1,5 @@
 # for the mailer
 SITE_URL = "ccstb.railsplayground.net"
 ADMIN_EMAIL = "dbadmin@#{SITE_URL}"
-APPLICATION_NAME = "CCStB"
+APPLICATION_NAME = "CCStB database"
 ORGANIZATION_NAME = "Community Center of St. Bernard"

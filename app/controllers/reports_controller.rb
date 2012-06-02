fc0d755386181955ec482662a6ef091884dbe146
Les Nightingill <codehacker@comcast.net>
2012-06-02 16:29:04 -0700

    now generating second harvest monthly report, but with fake data

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -7,9 +7,46 @@ class ReportsController < ApplicationController
     @month = Date::MONTHNAMES[1.month.ago.month]
     @parish = "Saint Bernard"
     @agency_name = "Community Center of St. Bernard"
-    @physical_address = "1111 LeBeau St., Arabi, LA 70032"
-    @contact_person = "Je'Nae Bailey"
+    @agency_number = "?"
+    @address = "1111 LeBeau St., Arabi, LA 70032"
+    @contact = "Je'Nae Bailey"
     @phone = "(504) 281-2512"
+    @days_and_hours = "T,W,Th, 10am - 2pm"
+    @demographic = {}
+    [:new, :continued, :total].each do |arg|
+      @demographic[arg] = {}
+      [:household, :children, :adults, :seniors, :homeless, :total].each do |cat|
+        @demographic[arg][cat] = rand(30).to_s
+      end
+    end
+
+    [:male, :female].each do |arg|
+      @demographic[arg] = {}
+      [:aa, :w, :hisp, :asian, :unk, :total].each do |cat|
+        @demographic[arg][cat] = rand(30).to_s
+      end
+    end
+    [:aa, :w, :hisp, :asian, :unk, :total].each do |cat|
+      @demographic[:total][cat] = rand(30).to_s
+    end
+
+    @number_of_boxes = rand(50).to_s
+    @average_weight = rand(12).to_s
+    @number_of_breakfasts = "N/A"
+    @number_of_lunches = "N/A"
+    @number_of_dinners = "N/A"
+    @number_of_snacks = "N/A"
+
+    @total_shfb_weight = rand(500)
+    @total_weight_purchased = rand(500)
+    @total_other_weight = rand(500)
+    @grand_total = (@total_shfb_weight + @total_weight_purchased + @total_other_weight).to_s
+    @total_shfb_weight = @total_shfb_weight.to_s
+    @total_weight_purchased = @total_weight_purchased.to_s
+    @total_other_weight = @total_other_weight.to_s
+
+    @questionnaires_count = rand(100).to_s
+
 
     respond_to do |format|
       format.docx {render :docx => "second_harvest_monthly", :template=> "document_templates/second_harvest_monthly_report", :from_template => true}

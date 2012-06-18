a34f27f50f1960c3dae69d6ff355c6437a4a56ba
Les Nightingill <codehacker@comcast.net>
2012-06-18 09:36:03 -0700

    move actionmailer smtp settings into separate file

---
+++ config/initializers/action_mailer.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+    :address => "localhost",
+    :port => 25,
+    :domain => "ccstb.railsplayground.net",
+    :authentication => :login,
+    :user_name => "dbadmin@ccstb.railsplayground.net",
+    :password => "ZFzZZpWf7Cq7Gk",
+    :enable_starttls_auto => false
+  }
